bf4aa2e1dcb2a88861d4d1cb45eb2ea9c29ea3fe
Jake Coombs <57802613+jakecoombs@users.noreply.github.com>
2021-11-27 12:15:24 +0000

    Check for migration FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -31,26 +31,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  void initState() {
-    Future.delayed(Duration.zero, () async {
-      /* CHECK FOR MIGRATION ON APP LOAD */
-      /* TODO: FUTUREBUILDER FOR MIGRATED */
-      bool migrated = await checkForMigration();
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
-          ),
-        ));
+        body: FutureBuilder<bool>(
+            future:
+                checkForMigration(), // a previously-obtained Future<String> or null
+            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+              List<Widget> children;
+              if (snapshot.hasData) {
+                /* IF SNAPSHOT.DATA THEN LAUNCH CALC WITH DIALOG
+                  ELSE JUST LAUNCH CALCULATOR SCREEN
+                 */
+                bool result = snapshot.data as bool;
+                children = <Widget>[
+                  Icon(
+                    Icons.check_circle_outline,
+                    color: result ? Colors.green : Colors.red,
+                    size: 60,
+                  )
+                ];
+              } else if (snapshot.hasError) {
+                children = <Widget>[
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  )
+                ];
+              } else {
+                /* CHECKING FOR MIGRATION */
+                children = const <Widget>[
+                  SizedBox(
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                    ),
+                    width: 60,
+                    height: 60,
+                  )
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            }));
   }
 }
