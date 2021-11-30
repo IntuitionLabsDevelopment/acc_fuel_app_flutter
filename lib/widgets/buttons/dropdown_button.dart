ee0d54185117bb3f947f09785dcf9991801866c6
Jake Coombs <57802613+jakecoombs@users.noreply.github.com>
2021-11-30 13:56:45 +0000

    Dropdown styling

--- lib/widgets/buttons/dropdown_button.dart
+++ lib/widgets/buttons/dropdown_button.dart
@@ -7,17 +7,25 @@ Widget dropdownButton({
   required void Function(dynamic) onChanged,
   Widget Function(dynamic)? optionMapper,
 }) {
-  return DropdownButton(
-    isExpanded: true,
-    value: value,
-    icon: const Icon(Icons.keyboard_arrow_down),
-    hint: Text(title),
-    onChanged: onChanged,
-    items: options.map<DropdownMenuItem<String>>((String value) {
-      return DropdownMenuItem<String>(
-        value: value,
-        child: optionMapper != null ? optionMapper(value) : Text(value),
-      );
-    }).toList(),
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 4),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(title),
+        DropdownButton(
+          isExpanded: true,
+          value: value,
+          icon: const Icon(Icons.keyboard_arrow_down),
+          onChanged: onChanged,
+          items: options.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: optionMapper != null ? optionMapper(value) : Text(value),
+            );
+          }).toList(),
+        ),
+      ],
+    ),
   );
 }
