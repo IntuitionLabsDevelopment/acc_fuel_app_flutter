b72138ed2856cb868631e46ca5c5be909282c3db
Jake Coombs <57802613+jakecoombs@users.noreply.github.com>
2021-12-02 17:34:19 +0000

    Dont show leave review ios

--- lib/widgets/buttons/leave_review_button.dart
+++ lib/widgets/buttons/leave_review_button.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:in_app_review/in_app_review.dart';
@@ -17,7 +19,7 @@ Widget leaveReviewButton(BuildContext context, {bool fullWidth = false}) {
       Widget child = const SizedBox.shrink();
       if (snapshot.hasData) {
         child = Visibility(
-          visible: snapshot.data as bool,
+          visible: snapshot.data as bool && Platform.isAndroid,
           child: ElevatedButton.icon(
               icon: const FaIcon(FontAwesomeIcons.starHalfAlt),
               style: fullWidth
@@ -27,7 +29,7 @@ Widget leaveReviewButton(BuildContext context, {bool fullWidth = false}) {
                   : null,
               onPressed: () async {
                 Navigator.of(context).pop();
-                inAppReview.openStoreListing(appStoreId: '1540153714');
+                inAppReview.openStoreListing();
               },
               label: Text(AppLocalizations.of(context)!.leaveReview)),
         );
